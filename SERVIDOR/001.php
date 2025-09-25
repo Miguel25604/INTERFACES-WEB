dfcd58ecbd61c583730e0c592d254cd8be2b71d2
Miguel Gavila Ibanez  -  Alumno CEAC FP <miguel.gavila@alu.ceacfp.es>
2025-09-25 16:31:06 +0200

    Subiendo una carpeta

---
+++ SERVIDOR/001.php
@@ -0,0 +1,7 @@
+<p>Un archivo PHP puede contener a HTML </p>
+
+    <?php
+        echo "Lo que escriba aqui dentro es PHP pero se convierte a HTML";
+    ?>
+
+<p>Puedo continuar con HTML.puro </p>
