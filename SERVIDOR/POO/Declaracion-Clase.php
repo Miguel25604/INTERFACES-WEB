2a68e59196068be3e83bde66f87d6d610008b8fa
Miguel Gavila Ibanez  -  Alumno CEAC FP <miguel.gavila@alu.ceacfp.es>
2025-10-06 16:17:56 +0200

    Actualizacion de la carpeta de servidor y una subcarpeta

--- SERVIDOR/POO/Declaracion-Clase.php
+++ SERVIDOR/POO/Declaracion-Clase.php
@@ -20,3 +20,22 @@
     }
 
 ?>
+GI
+<?php
+
+    class Entrada{
+        function __construct(){
+            $this->titulo = "";
+            $this->fecha = "";
+            $this->autor = "";
+            $this->contenido = "";
+            $this->imagen = "";
+        }
+        
+    }
+
+    $entrada1 = new Entrada();
+    var_dump($entrada1);
+    $entrada2 = new Entrada();
+    var_dump($entrada2);
+?>
