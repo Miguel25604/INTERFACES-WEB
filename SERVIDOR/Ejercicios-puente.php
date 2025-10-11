333b93c089d7fae9e08cc21fc92e5bc976c5cbfc
Miguel25604 <miguel.gavila@alu.ceacfp.es>
2025-10-11 12:09:14 +0200

    Creación de ejercicio puente

--- SERVIDOR/Ejercicios-puente.php
+++ SERVIDOR/Ejercicios-puente.php
@@ -1,29 +1,33 @@
 <?php
-
-    /* Recibir 4 números por la URL y mostrarlos por pantalla. */
-    $n1 = $_GET['n1'];
-    $n2 = $_GET['n2'];
-    $n3 = $_GET['n3'];
-    $n4 = $_GET['n4'];
-
-
-    echo "Número 1: $n1 <br>";
-    echo "Número 2: $n2 <br>";
-    echo "Número 3: $n3 <br>";
-    echo "Número 4: $n4 <br>";
-
-
-?>
-
-<?php
-    /* ARRAY VACIO */
     
+        /* Recibir 4 números por la URL y mostrarlos por pantalla. */
+    /*
+    
+        n1 = $_GET['n1'];
+        $n2 = $_GET['n2'];
+        $n3 = $_GET['n3'];
+        $n4 = $_GET['n4'];
+
+        
+
+        echo "Número 1: $n1 <br>";
+        echo "Número 2: $n2 <br>";
+        echo "Número 3: $n3 <br>";
+        echo "Número 4: $n4 <br>";
+    */
+
+
+
+
+
+    /* ARRAY VACIO */
+    /*
     $numeros = array();
+    */
 
 
-?>
 
-<?php
+
 
     /* ARRAY TODOS LOS NUMEROS AL FINAL DEL ARRAY */
     $numeros = array();
@@ -34,5 +38,19 @@
     $numeros[] = 16;
     
     /*MOSTRAMOS EL ARRAY */
+    var_dump($numeros);
+
+
+    echo "Array original: <br>";
+    var_dump($numeros);
+    echo "<br>";
+
+    $ultimo = array_pop($numeros);
+    echo "Array despues de eliminar el ultimo elemento ($ultimo): <br>";
+
+    echo "Numero eliminado: $ultimo <br>";
+    var_dump($numeros);
+
+    echo "Array despues de eliminar el ultimo elemento ($ultimo): <br>";
     var_dump($numeros);
 ?>
