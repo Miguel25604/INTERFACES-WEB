6dc27402d14048c6940eb80d52d89f536ca76062
Miguel25604 <miguel.gavila@alu.ceacfp.es>
2025-10-11 11:27:52 +0200

    Creación de ejercicio puente

--- SERVIDOR/Ejercicios-puente.php
+++ SERVIDOR/Ejercicios-puente.php
@@ -6,11 +6,33 @@
     $n3 = $_GET['n3'];
     $n4 = $_GET['n4'];
 
-    
+
     echo "Número 1: $n1 <br>";
     echo "Número 2: $n2 <br>";
     echo "Número 3: $n3 <br>";
     echo "Número 4: $n4 <br>";
 
 
+?>
+
+<?php
+    /* ARRAY VACIO */
+    
+    $numeros = array();
+
+
+?>
+
+<?php
+
+    /* ARRAY TODOS LOS NUMEROS AL FINAL DEL ARRAY */
+    $numeros = array();
+
+    $numeros[] = 4;
+    $numeros[] = 8;
+    $numeros[] = 15;
+    $numeros[] = 16;
+    
+    /*MOSTRAMOS EL ARRAY */
+    var_dump($numeros);
 ?>
