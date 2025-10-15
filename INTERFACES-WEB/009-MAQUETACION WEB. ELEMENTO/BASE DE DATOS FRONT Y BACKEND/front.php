1b8d71ccab579a4a873fb62e026be6b489b0a687
Miguel25604 <miguel.gavila@alu.ceacfp.es>
2025-10-15 19:02:18 +0200

    Creacion de subcarpeta llamada Base de datos y archivos php

---
+++ INTERFACES-WEB/009-MAQUETACION WEB. ELEMENTO/BASE DE DATOS FRONT Y BACKEND/front.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Miguelpress</title>
+    <style>
+      html,
+      body {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        padding: 0px;
+        margin: 0px;
+        font-family: "Lucida Sans", Arial, sans-serif;
+        background: steelblue;
+      }
+      header,
+      footer,
+      main {
+        background: white;
+        padding: 20px;
+        width: 600px;
+        margin: auto;
+      }
+      article {
+        padding: 20px;
+        border-bottom: 1px solid steelblue;
+      }
+      article * {
+        padding: 5px;
+        margin: 0px;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+        <h1>El blog de Miguel Gavilá</h1>
+    </header>
+    <main>
+      <?php
+        $basededatos = new SQLite3("miguel.db");
+        $resultados = $basededatos->query("SELECT * FROM post");
+        while ($fila = $resultados->fetchArray()) {
+            echo "<article>";
+            echo "<h2>" . $fila['titulo'] . "</h2>";
+            echo "<p>" . $fila['fecha'] . "</p>";
+            echo "<div>" . $fila['contenido'] . "</div>";
+            echo "</article>";
+        }
+
+        $basededatos->close();
+        ?>
+
+    </main>
+    <footer>Copyright 2024 - Miguel Gavilá Ibáñez</footer>
+    </body>
+</html>
