780d66b23142c2d10943f7c5ff2c8797d23a6941
Miguel25604 <miguel.gavila@alu.ceacfp.es>
2025-10-23 18:20:02 +0200

    modificacion

--- SERVIDOR/colaEjercicio.php
+++ SERVIDOR/colaEjercicio.php
@@ -13,14 +13,11 @@ $pedidos_espera->enqueue("Pedido 1: Hamburguesas con bacon");
 $pedidos_espera->enqueue("Pedido 2: Chuletas ");
 $pedidos_espera->enqueue("Pedido 3: Arroz al horno");
 
-echo "Pedidos en espera:\n<br><br>";
+echo "<h1> Pedidos en espera:\n<h1> <br><br>";
+const CAPACIDAD = 3;
 
-if (!$pedidos_espera->isEmpty()) {
-    echo "Primer pedido en espera: " . $pedidos_espera->bottom() . "<br><br>";
-}
-function hayHueco(SplQueue $q, int $cap) {
-    return $q->count() < $cap; // menor que 3
-}
+
+
 
 
 ?>
